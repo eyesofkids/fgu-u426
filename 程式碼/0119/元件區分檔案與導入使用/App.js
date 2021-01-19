f85c7e77570c99defefc84e71247f4be4a5069ba
Eddy Chang <eddy@joomla.com.tw>
2021-01-19 13:21:11 +0800

    add

---
+++ 程式碼/0119/元件區分檔案與導入使用/App.js
@@ -0,0 +1,14 @@
+import CountFunc from './components/CountFunc'
+import CountClass from './components/CountClass'
+
+function App() {
+  return (
+    <>
+      <CountClass />
+      <hr />
+      <CountFunc />
+    </>
+  )
+}
+
+export default App
---
+++ 程式碼/0119/元件區分檔案與導入使用/components/CountClass.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+class CountClass extends React.Component {
+  constructor() {
+    super()
+    this.state = {}
+  }
+
+  render() {
+    return <h1>0</h1>
+  }
+}
+
+export default CountClass
