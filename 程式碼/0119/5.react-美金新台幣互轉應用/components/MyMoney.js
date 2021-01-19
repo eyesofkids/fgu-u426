abc23e2a206779f9f19d1615a65d658bdb20eb98
Eddy Chang <eddy@joomla.com.tw>
2021-01-19 13:55:36 +0800

    add

---
+++ 程式碼/0119/5.react-美金新台幣互轉應用/components/MyMoney.js
@@ -0,0 +1,13 @@
+function MyMoney() {
+  return (
+    <>
+      <h1>貨幣轉換程式</h1>
+      <input type="text" />
+      <button>新台幣轉美金</button>
+      <button>美金轉新台幣</button>
+      <h1>最後轉換為xxxx元</h1>
+    </>
+  )
+}
+
+export default MyMoney
