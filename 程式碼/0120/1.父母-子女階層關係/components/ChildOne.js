69ecd9d0dd2118cd2c032c056c49a8960631c615
Eddy Chang <eddy@joomla.com.tw>
2021-01-20 09:37:42 +0800

    add

---
+++ 程式碼/0120/1.父母-子女階層關係/components/ChildOne.js
@@ -0,0 +1,9 @@
+function ChildOne() {
+  return (
+    <>
+      <h2>ChildOne</h2>
+    </>
+  )
+}
+
+export default ChildOne
---
+++ 程式碼/0120/1.父母-子女階層關係/components/Parent.js
@@ -0,0 +1,12 @@
+import ChildOne from './ChildOne'
+
+function Parent() {
+  return (
+    <>
+      <h2>Parent</h2>
+      <ChildOne />
+    </>
+  )
+}
+
+export default Parent
